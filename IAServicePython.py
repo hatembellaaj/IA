5824c2c0a681fd9901dd728b6fbba4152bb10a9f
= <=>
2023-05-20 07:36:20 +0100

    first commit

---
+++ IAServicePython.py
@@ -0,0 +1,37 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+os.environ['AUTOGRAPH_VERBOSITY'] = '1'
+#import tensorflow as tf
+import tensorflow 
+import numpy as np
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense
+import logging
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+tensorflow.autograph.set_verbosity(0)
+#from tn.mdweb.dsi.tfar import IAService
+
+#class IAServicePython(IAService):
+class IAServicePython():
+    a1=""
+    def preduction_keras(self):
+        dataset=np.genfromtxt('matTab.csv',delimiter=',',skip_header=True,usecols=(1,6,7))
+        model = Sequential()
+        #model.compile(loss='mse', optimizer='rmsprop', metrics=['mse'])
+        model.compile(loss='mse', optimizer='rmsprop', metrics=['accuracy'])
+        model.add(Dense(12, input_dim=2, activation='relu'))
+        model.add(Dense(8, activation='relu'))
+        model.add(Dense(1, activation='linear'))
+        x=dataset[:,0:2]
+        y=dataset[:,2]
+        model.fit(x,y,epochs=100)
+        predictions=model.predict(x)
+        for i in range(5):
+            #print('%s => %d (expected %d)' % (x[i].tolist(), predictions[i], y[i]))
+            self.a1=self.a1+"{a} => {b} (expected {c})\n".format(a=x[i].tolist(), b=predictions[i], c=y[i])
+        
+        return self.a1;
+
+i=IAServicePython();
+print(i.preduction_keras())
+
