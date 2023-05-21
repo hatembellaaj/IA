2012f107eafe7440cc97a79193382a9980f0d2e2
= <=>
2023-05-21 19:16:28 +0100

    more tests

--- witharguments.py
+++ witharguments.py
@@ -2,3 +2,7 @@ import sys
 
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
+T=[];
+T.append(int(sys.argv[1]))
+T.append(int(sys.argv[2]))
+print(T)
